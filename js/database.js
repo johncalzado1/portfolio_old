012e2bd1416ab28ad7cd5238ebddb0acc5e917cf
golars497 <john_lyndonne_calzado497@hotmail.co.uk>
2018-06-18 22:14:32 +0100

    propject renameject rename

---
+++ js/database.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBTMX40czfTG04TWspGuzMwhtEvB6IZJfw",
+    authDomain: "johncalzado1.firebaseapp.com",
+    databaseURL: "https://johncalzado1.firebaseio.com",
+    projectId: "johncalzado1",
+    storageBucket: "",
+    messagingSenderId: "198600549918"
+  };
+  firebase.initializeApp(config);
